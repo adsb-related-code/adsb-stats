066bb98e4046650b0d5b0dba98e15811b296cb4c
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2019-11-01 08:38:54 -0700

    Update install.sh
    sudo apt-get install -y curl uuid-runtime jq inotify-tool gzip

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 # requires uuidgen curl
 sudo apt-get update
-sudo apt-get install -y curl uuid-runtime
+sudo apt-get install -y curl uuid-runtime jq inotify-tool gzip
 
 #copy the service file
 cp adsbexchange-stats.service /etc/systemd/system/adsbexchange-stats.service
