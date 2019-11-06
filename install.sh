1d2b08cc09f5dcaba164049564af17a147e74f50
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2019-11-06 15:47:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,23 @@ set -e
 sudo apt-get update
 sudo apt-get install -y curl uuid-runtime jq inotify-tools gzip
 
+# create uuid
+UUID_FILE="/boot/adsbx-uuid"
+# look for uuid - if invalid or missing genrate one
+if [ -s $UUID_FILE ]; then
+        UUID=$(cat $UUID_FILE)
+        if [[ $UUID =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
+                echo '1'
+        else
+                rm -f $UUID_FILE
+                UUID=$(uuidgen -t)
+                echo $UUID > $UUID_FILE
+        fi
+else
+        UUID=$(uuidgen -t)
+        echo $UUID > $UUID_FILE
+fi
+
 #copy the service file
 cp adsbexchange-stats.service /etc/systemd/system/adsbexchange-stats.service
 
